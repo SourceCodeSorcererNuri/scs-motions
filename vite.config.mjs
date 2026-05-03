444e95f9464d5c569c7a1b2460219809d28ad50d
SourceCodeSorcererNuri <nuriddinabduraxmonov842@gmail.com>
2026-05-03 15:50:13 +0500

    Refactor vite.config.mjs to simplify configuration
    Removed unnecessary export and plugin configuration.

--- vite.config.mjs
+++ vite.config.mjs
@@ -16,19 +16,4 @@ export default defineConfig({
             "Cross-Origin-Embedder-Policy": "require-corp",
         },
     }
-});import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        allowedHosts: [
-            '.ngrok-free.app',
-            'localhost'
-        ],
-        headers: {
-            "Cross-Origin-Opener-Policy": "same-origin",
-            "Cross-Origin-Embedder-Policy": "require-corp",
-        },
-    }
 });
